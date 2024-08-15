3f46cf07ec65e534f9292c1a125acb102e0d62af
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-08-15 20:15:45 +0530

    [COHORT3.0:WEEK02:DAY02]: Added class file.

--- Week-02/DAY-02/Covered-Topics/Concepts/class.js
+++ Week-02/DAY-02/Covered-Topics/Concepts/class.js
@@ -46,5 +46,80 @@ class Rectangle {
 const rect = new Rectangle(22, 11);
 const area = rect.area();
 const perimeter = rect.perimeter();
-console.log("Area of rectangle:", area);
-console.log("Perimeter of rectangle:", perimeter);
+// console.log("Area of rectangle:", area);
+// console.log("Perimeter of rectangle:", perimeter);
+
+/*
+    Inheritence in Classes
+    - To create a class inheritance, use the "extends" keyword.
+    - A class created with a class inheritance inherits all the methods from another class.
+ */
+
+// Creating a base shape class.
+
+class Shape {
+    constructor(color) {
+        this.color = color;
+    }
+    paint() {
+        console.log(`Painting with color ${this.color}.`);
+    }
+    area() {
+        throw new Error("The area method must be implemented in the subclass.");
+    }
+    perimeter() {
+        throw new Error("The perimeter method must be implemented in the subclass.");
+    }
+    getDescription() {
+        return `A shape with color ${this.color}.`;
+    }
+}
+
+class Circle extends Shape {
+    constructor(radius, color) {
+        super(color);
+        this.radius = radius;
+    }
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+    perimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+    getDescription() {
+        return `A circle with radius ${this.radius} and color ${(this.color)}.`;
+    }
+    paint() {
+        return `Painting with color ${this.color}.`;
+    }
+}
+const c1 = new Circle(3, "blue");
+// console.log("The area of Circle:", c1.area());
+// console.log("The perimeter of Circle:", c1.perimeter());
+// console.log("About Circle:", c1.getDescription());
+// console.log("Color of Circle:", c1.paint());
+
+class Square extends Shape {
+    constructor(side, color) {
+        super(color);
+        this.side = side;
+    }
+    area() {
+        return this.side * this.side;
+    }
+    perimeter() {
+        return 4 * this.side;
+    }
+    getDescription() {
+        return `A Square with side ${this.side} and color ${this.color}.`;
+    }
+    paint() {
+        return `Painting with color ${this.color}.`;
+    }
+}
+const s1 = new Square(5, "red");
+// console.log("The area of Square:", s1.area());
+// console.log("The perimeter of Square:", s1.perimeter());
+// console.log("About Square:", s1.getDescription());
+// console.log("Color of Square:", s1.paint());
+
