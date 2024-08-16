f5c2929e9310d73c2309753ea4f29f988c7124b6
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-08-16 13:18:49 +0530

    [COHORT3.0:Web-Dev:Week02:Day01]: Updated sync.js

--- Web-Dev/Week-02/DAY-01/Covered-Topics/Concepts/sync.js
+++ Web-Dev/Week-02/DAY-01/Covered-Topics/Concepts/sync.js
@@ -3,6 +3,20 @@
 function sum(number) {
     return (number * (number + 1)) / 2;
 }
-console.log("Sum of 5 numbers:", sum(5));
-console.log("Sum of 10 numbers:", sum(10));
-console.log("Sum of 100 number:", sum(100));
+// console.log("Sum of 5 numbers:", sum(5));
+// console.log("Sum of 10 numbers:", sum(10));
+// console.log("Sum of 100 number:", sum(100));
+
+
+function func() {
+    console.log(`"Task02" inside sync function.`);
+}
+
+// Task01
+console.log(`"Task01" start.`);
+
+// Task02
+func();
+
+// Task03
+console.log(`"Task03 end.`);
