57c78308d647b5172f9ae5e2deec7b1119247387
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-08-16 16:19:43 +0530

    [COHORT3.0:Week02:Day01]: Updated async.js

--- Web-Dev/Week-02/DAY-01/Covered-Topics/Concepts/async.js
+++ Web-Dev/Week-02/DAY-01/Covered-Topics/Concepts/async.js
@@ -8,3 +8,14 @@ If the code consists any of the following, the code is "async", otherwise "sync"
 NOTE: WEB API's: setTimeout() \\ setInterval() \\ promises \\ fetch \\ axios \\ XMLHttpRequest \\ Geolocation API \\ 
     Web storage API || URL API, etc.
 */
+
+// Task01
+console.log(`"Task01"-start.`);
+
+// Task02
+setTimeout(() => {
+    console.log(`"Task02"-inside setTimeout function.`);
+}, 2000);
+
+// Task03
+console.log(`"Task03"-end.`);
