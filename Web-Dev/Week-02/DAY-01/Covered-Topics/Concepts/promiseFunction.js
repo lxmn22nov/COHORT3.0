3f56d15d1b0b42432ab6f051990ff10a27ad33ed
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-08-17 22:59:48 +0530

    [COHORT3.0:Week02:Day01]: Updated promiseFunction.

--- Web-Dev/Week-02/DAY-01/Covered-Topics/Concepts/promiseFunction.js
+++ Web-Dev/Week-02/DAY-01/Covered-Topics/Concepts/promiseFunction.js
@@ -1,7 +1,14 @@
 /*
     PROMISE:
-    - Introduced to address the issues of callback hell, providing more  manageable way to handle async operations.
+    - Introduced to address the issues of callback hell, providing more manageable way to handle async operations.
 */
+
+// Creating Promise.
+
+new Promise((resolve, reject) => {
+    // async code
+});
+
 
 function task1() {
     return new Promise((resolve) => {
@@ -30,9 +37,29 @@ function task3() {
     })
 }
 
-step1()
-    .then(step2)
-    .then(step3)
-    .then(() => {
-        console.log("All Task completed.");
-    });
+// step1()
+//     .then(step2)
+//     .then(step3)
+//     .then(() => {
+//         console.log("All Task completed.");
+//     });
+
+function checkEven(number) {
+    return new Promise((resolve, reject) => {
+        if (number % 2 === 0) {
+            resolve("Yes, it's even number.");
+        } else {
+            reject("No, it's odd number.");
+        }
+    })
+}
+
+// console.log("The given number is:", checkEven(22));
+// console.log("The given number is:", checkEven(21));
+
+checkEven(12).then((res) => {
+    console.log(res);
+}).catch((error) => {
+    console.log(error);
+});
+
