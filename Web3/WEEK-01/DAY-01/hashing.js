dc1355f429c57523eb8f53c97c42aec65af8cd2a
Laxman Singh Koranga <laxmankoranga03@gmail.com>
2024-08-23 20:44:32 +0530

    Updated local repo to remote.

--- Web3/WEEK-01/DAY-01/hashing.js
+++
@@ -1,8 +0,0 @@
-// Hashing function using node.js
-
-const crypto = require("crypto");
-
-const input = "Laxman";
-const hash = crypto.createHash("sha256").update(input).digest("hex");
-
-console.log("Hash String \"Laxman\":", hash);
